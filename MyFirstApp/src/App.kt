e7e9eae551c93cd2218e94d1268d83e00b994c78
George <dirtyqbn@gmail.com>
2024-09-24 21:18:08 -0700

    Chapter 1 done
    Just adding some notes

--- MyFirstApp/src/App.kt
+++ MyFirstApp/src/App.kt
@@ -1,34 +1,11 @@
 fun main (args: Array<String>) {
-    println("Pow!")
+    var x = 1
 
-    var x = 3
-    val name = "Cormoron"
-    x = x * 10
-    println("x is $x.")
-    // this comment
-
-    while (x > 20) {
-        x = x - 1
-        println(" x is now $x.")
+    while (x < 3) {
+        print(if (x == 1) "Yab" else  "Dab")
+        print("ba")
+        x = x + 1
     }
-    println()
-    println()
-
-    for (i in 1..10) {
-        x = x +1
-        println(" x is now $x.")
-    }
-
-    println()
-    println()
-
-   if (x == 20) {
-        println(" x must be 20.")
-    } else {
-        println(" x isn't 20.")
-   }
-
-    if (name.equals("Cormoron")) {
-        println("$name Strike")
-    }
+    if (x == 3) println("Do")
 }
+
