2bd94c2f3a8ea2fbd79b88ed05371fdd358cc0c8
George <dirtyqbn@gmail.com>
2024-09-24 20:22:22 -0700

    Update App.kt
    Quick update only from GH desktop app

--- MyFirstApp/src/App.kt
+++ MyFirstApp/src/App.kt
@@ -1,3 +1,34 @@
 fun main (args: Array<String>) {
     println("Pow!")
+
+    var x = 3
+    val name = "Cormoron"
+    x = x * 10
+    println("x is $x.")
+    // this comment
+
+    while (x > 20) {
+        x = x - 1
+        println(" x is now $x.")
+    }
+    println()
+    println()
+
+    for (i in 1..10) {
+        x = x +1
+        println(" x is now $x.")
+    }
+
+    println()
+    println()
+
+   if (x == 20) {
+        println(" x must be 20.")
+    } else {
+        println(" x isn't 20.")
+   }
+
+    if (name.equals("Cormoron")) {
+        println("$name Strike")
+    }
 }
