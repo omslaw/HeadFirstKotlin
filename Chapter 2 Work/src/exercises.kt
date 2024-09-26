2adc3169f1f73884320e92f5af8863999619ddd4
George <dirtyqbn@gmail.com>
2024-09-25 20:21:07 -0700

    Chapter 2 Work
    Left of on page 117

---
+++ Chapter 2 Work/src/exercises.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+
+    // Sharpen your pencil Exercise
+
+    //var x: Int = 65.2  // not an int BAD
+    var isPunk = true //should be caps?
+    //var message = 'Hello' // should be char single ' not double for string "
+    var y = 7 // its int
+
+    // ones that dont work
+    // b = smallNum // wont work since b is byte and smallnum was a short
+    // isPunk = "false" // wont work since this should be boolean not string
+    // var k = y.toDouble()  // y is 57 int
+    // b = k.toByte() // byte is good up to 127 so should be fine
+}
