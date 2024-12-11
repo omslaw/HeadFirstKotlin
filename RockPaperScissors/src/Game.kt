b0384bbcd7be2166a2310c4c604d475cfc3a0888
George <dirtyqbn@gmail.com>
2024-12-10 22:40:14 -0800

    Chap 3 work
    Left off page 160

--- RockPaperScissors/src/Game.kt
+++ RockPaperScissors/src/Game.kt
@@ -6,5 +6,7 @@ fun main(args: Array<String>) {
 fun getGameChoice (optionsParam: Array<String>) =
     optionsParam [(Math.random() * optionsParam.size).toInt()]
 
-        //temp
+fun getUserChoice(optionsParam: Array<String>): String {
+  // need to finish, left of page 160
+}
 
