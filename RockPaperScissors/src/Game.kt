2d86faca6ebe867b1d0bab18a18a9da6d8a51f74
George <dirtyqbn@gmail.com>
2024-12-11 22:26:45 -0800

    Update Game.kt

--- RockPaperScissors/src/Game.kt
+++ RockPaperScissors/src/Game.kt
@@ -7,6 +7,12 @@ fun getGameChoice (optionsParam: Array<String>) =
     optionsParam [(Math.random() * optionsParam.size).toInt()]
 
 fun getUserChoice(optionsParam: Array<String>): String {
-  // need to finish, left of page 160
+    // Ask the user for their choice
+    print("Please enter one of the following:")
+    for (item in optionsParam) print(" $item")
+    println(".")
+    // Read the user input
+    val userInput = readLine()
+
 }
 
