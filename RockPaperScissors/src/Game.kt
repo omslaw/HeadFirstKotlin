cea36d4bed0e3907a19751502c683eeed6315da4
George <dirtyqbn@gmail.com>
2024-12-14 18:40:01 -0800

    Chap 3 update
    left at pool puzzle page 171

--- RockPaperScissors/src/Game.kt
+++ RockPaperScissors/src/Game.kt
@@ -1,18 +1,33 @@
 fun main(args: Array<String>) {
     val options = arrayOf("Rock", "Paper", "Scissors")
     val gameChoice = getGameChoice(options)
+    val userChoice = getUserChoice(options)
 }
 
 fun getGameChoice (optionsParam: Array<String>) =
     optionsParam [(Math.random() * optionsParam.size).toInt()]
 
 fun getUserChoice(optionsParam: Array<String>): String {
-    // Ask the user for their choice
-    print("Please enter one of the following:")
-    for (item in optionsParam) print(" $item")
-    println(".")
-    // Read the user input
-    val userInput = readLine()
+    var isValidChoice = false
+    var userChoice = ""
+    // Loop until the user enters a valid choice
+    while (!isValidChoice) {
+        // Ask the user for their choice
+        print("Please enter one of the following:")
+        for (item in optionsParam) print(" $item")
+        println(".")
+        // Read the user input
+        val userInput = readLine()
+        // Validate the user input
+        if (userInput != null && userInput in optionsParam) {
+            isValidChoice = true
+            userChoice = userInput
+        }
+        // If the choice is invalid, inform the user
+        if (!isValidChoice) println("You must enter a valid choice.")
+
+    }
+    return userChoice
 
 }
 
