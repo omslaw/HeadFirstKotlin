4cd5d93c7f65db4a475cfa325a272d575de8df66
George <dirtyqbn@gmail.com>
2024-12-15 16:39:36 -0800

    Chap 3 Complete

--- RockPaperScissors/src/Game.kt
+++ RockPaperScissors/src/Game.kt
@@ -2,6 +2,7 @@ fun main(args: Array<String>) {
     val options = arrayOf("Rock", "Paper", "Scissors")
     val gameChoice = getGameChoice(options)
     val userChoice = getUserChoice(options)
+    printResult(userChoice, gameChoice)
 }
 
 fun getGameChoice (optionsParam: Array<String>) =
@@ -17,7 +18,11 @@ fun getUserChoice(optionsParam: Array<String>): String {
         for (item in optionsParam) print(" $item")
         println(".")
         // Read the user input
-        val userInput = readLine()
+        var userInput = readLine()
+        if (userInput != null) {
+            userInput = userInput.capitalize()
+        }
+        
         // Validate the user input
         if (userInput != null && userInput in optionsParam) {
             isValidChoice = true
@@ -28,6 +33,19 @@ fun getUserChoice(optionsParam: Array<String>): String {
 
     }
     return userChoice
+}
 
+fun printResult (userChoice: String, gameChoice: String) {
+    val result: String
+
+    //Figure out the result
+    if (userChoice == gameChoice) result = "Tie!"
+    else if ((userChoice == "Rock" && gameChoice == "Scissors") ||
+            (userChoice == "Paper" && gameChoice == "Rock" ) ||
+            (userChoice == "Scissors" && gameChoice == "Paper")) result = "You win!"
+    else result = "You lose!"
+
+    // Print the result
+    println ("You chose $userChoice. I chose $gameChoice. $result")
 }
 
