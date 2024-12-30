fde3bbde2e9617a9ec65f4411651693c11722d0c
George <dirtyqbn@gmail.com>
2024-12-29 19:59:34 -0800

    Working on code magnets exercise page 195

---
+++ Songs/Chapter_4_Notes.kt
@@ -0,0 +1,25 @@
+///--- Code Magnets Exercise  ----
+
+//outputs
+//ding ding ba-da-bing!
+//bang bang bang!
+//ding ding ba-da-bing!
+
+
+class DrumKit (var hasTopHat: Boolean, var hasSnare: Boolean) {
+    fun playTopHat() {
+        println ("ding ding ba-da-bing!")
+    }
+
+    fun playSnare() {
+        println ("bang bang bang!")
+    }
+
+
+
+}
+
+fun main(args: Array<String>) {
+
+}
+
